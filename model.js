5834fa57fb9c948168c367a482b8c6bcd351e877
Jim Shelly <jim.shelly@gmail.com>
2020-01-05 17:26:12 +0000

    Added Observable Pattern

--- model.js
+++ model.js
@@ -1,3 +1,30 @@
 function Model() {
-  this.heading = 'Hello';
+  var self = this;
+  //heading is no longer a property, but a scoped variable
+  var heading = 'Hello';
+
+  //collection of observers
+  this.observers = [];
+
+  //add to the collection of observers
+  this.registerObserver = function(observer) {
+    self.observers.push(observer);
+  }
+
+  //loop over observers, calling their update method
+  this.notifyAll = function() {
+    self.observers.forEach(function(observer){
+      observer.update(self);
+    })
+  }
+
+  //Pass this, as its the object we want to affect.  Heading is the name of the property we want it to be attached to.  Then we defined the accessor and assignment functions
+  Object.defineProperty(this, 'heading', {
+    get: function() { return heading },
+    set: function(value) {
+      heading = value;
+      //call notifyAll in the assignment function
+      this.notifyAll();
+    }
+  })
 }
--- view.js
+++ view.js
@@ -3,4 +3,8 @@ function View(controller) {
   this.heading = document.getElementById('heading');
   this.heading.innterText = controller.getModelHeading();
   this.heading.addEventListener('click', controller);
+  this.update = function(data){
+    this.heading.innerText = data.heading;
+  }
+  thos.controller.model.registerObserver(this);
 }
